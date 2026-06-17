386f88904c9cb600e5a528f3355044eb16ccc87c
newJoelQuintero <kevynn_quintero@soy.sena.edu.co>
2026-06-16 22:35:00 -0500

    Update settings.py

--- neuro_esencia/settings.py
+++ neuro_esencia/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'CdAQvdfZjQWYUdgakwhpErTFwdLehAwE',
+        'PASSWORD': 'CdAQVdfZjQWYUdgakWhpErTFwdLehAwE',
         'HOST': 'mysql.railway.internal',
         'PORT': '3306',
     }
