5818fa2beabc209895254753570f2957ee1ac312
newJoelQuintero <kevynn_quintero@soy.sena.edu.co>
2026-06-16 22:18:31 -0500

    Update settings.py

--- neuro_esencia/settings.py
+++ neuro_esencia/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'neuro_esencia.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQLDATABASE'),
-        'USER': os.getenv('MYSQLUSER'),
-        'PASSWORD': os.getenv('MYSQLPASSWORD'),
-        'HOST': os.getenv('MYSQLHOST'),
-        'PORT': os.getenv('MYSQLPORT'),
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST'),
+        'PORT': os.getenv('MYSQL_PORT'),
     }
 }
 
